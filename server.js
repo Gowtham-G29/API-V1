525808f90cd61d2dd795d2b3540eb81ec9870fec
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-07 14:29:16 +0530

    update: uncomment

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ mongoose.connect(DB, {
 
 })
     .then(() => {
-        // console.log("DB connection is established");
+        console.log("DB connection is established");
     })
     .catch((err) => {
-        // console.log('ERR: ', err);
+        console.log('ERR: ', err);
     });
 
 
@@ -25,5 +25,5 @@ const app = require('./app');
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    // console.log(`App running on port ${port}`);
+    console.log(`App running on port ${port}`);
 });
